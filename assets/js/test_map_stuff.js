390157e668d63005528199141ce504a75b06240a
Mitchell <tmitchell@cdsvisual.com>
2023-11-28 20:12:27 -0700

    added js node js files

---
+++ assets/js/test_map_stuff.js
@@ -0,0 +1,9 @@
+
+
+console.log("test")
+
+// Reading the file using default
+// fs npm package
+import csvToJson from '../docs/node_modules/convert-csv-to-json/src/csvToJson';
+let inputFile = '../docs/phzm_us_zipcode_2023.csv'
+
