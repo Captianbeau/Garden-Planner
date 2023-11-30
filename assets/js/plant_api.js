14c3634df4787fbe9e9555c2a22ebf7d79b36a8f
williamrankin <william.rankin01@gmail.com>
2023-11-30 16:05:43 -0700

    updated JS to allow plant search and to display plant card

--- assets/js/plant_api.js
+++ assets/js/plant_api.js
@@ -1,56 +1,81 @@
 
-
-//Set request option variable
+plantKey = "sk-5oBn655d7588a54043071";
+pageNum = 1;
+searchButton = document.getElementById("searchButton");
+var searchResults = document.querySelector("#searchResults");
 var requestOptions = {
   method: 'GET',
   redirect: 'follow',
 };
-//Fetch Plant API, not the queries in the URL to get desired search results
-fetch(`https://perenual.com/api/species-list?key=sk-5oBn655d7588a54043071&edible=1&hardiness=1-13&page=1`, requestOptions)
-  //Parse  
-  .then(response => response.json())
-  //Check if there is a result, then console log result and run function  
-  .then(result => {
-    console.log(result);
-    if (result.data && result.data.length > 0) {
-      displayPlants(result);
-      displayPlantCard(result);
-      //console.log(result.data[0].common_name)
-    } else {
-      console.log('No results found.');
-    }
-  })
-  .catch(error => console.log('error', error));
+
+
+searchButton.addEventListener("click", function (event) {
+
+  event.preventDefault()
+
+  searchParameter = document.getElementById("searchInput");
+
+  //window.location.href = "./results.html"
+
+  console.log(searchParameter.value);
+  searchResults.innerHTML = ""
+  searchPlantURL = "https://perenual.com/api/species-list?key=" + plantKey + "&q=" + searchParameter.value + "&edible=1&hardiness=1-13&page=" + pageNum;
+
+  fetch(searchPlantURL, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      if (result.data && result.data.length > 0) {
+        displayPlants(result);
+      } else {
+        console.log('No results found.');
+      }
+    });
+})
+
+
+
 
 //Create function to display plants on search page
 var displayPlants = function (result) {
   for (var i = 0; i < result.data.length; i++) {
+    if (result.data[i].id < 3001) {
     //connect to HTML element
-    var searchResults = document.querySelector("#searchResults");
+
     //pull common name from data
-    var plantCommonName = result.data[i].common_name;
-    console.log(plantCommonName)
+    var plantCommonName = result.data[i].common_name
+    var plantID = result.data[i].id
+console.log(result.data[i].id)
     //create li element on search page
     var resultItem = document.createElement("li")
+    resultItem.setAttribute("class", "list-item")
     //create text for li
-    resultItem.textContent = plantCommonName
+    resultItem.textContent = plantCommonName + ", ID= " +plantID;
     //append li to ul in the aside
     searchResults.appendChild(resultItem)
-  }
+  }}
 }
 
-var displayPlantCard = function (result) {
-  //connect to HTML element
-  var plantCard = document.querySelector("#plantCard");
-  //pull common name from data
-  var plantImage = document.querySelector("#plantImage");
-  console.log(plantImage)
-  var plantImg = result.data[0].default_image.regular_url;
-  console.log(result.data[0].default_image.regular_url)
-  //create li element on search page
-  var plantImgEl = document.createElement("img")
-  //create text for li
-  plantImgEl.setAttribute("src", plantImg)
-  //append li to ul in the aside
-  plantImage.append(plantImgEl)
-}
+
+searchResults.addEventListener("click", function (event) {
+  console.log(plantID)
+})
+
+
+
+//plantURL = "https://perenual.com/api/species-list?key=" + plantKey + "&edible=1&hardiness=1-13&page=" + pageNum;
+
+//browsePlantURL = "https://perenual.com/api/species-list?key=" + plantKey + "&edible=1&hardiness=1-13&page=" + pageNum;
+//Set request option variable
+
+//searchPlantURL = "https://perenual.com/api/species-list?key=" + plantKey + "&q=" + searchParameter + "&edible=1&hardiness=1-13&page=" + pageNum;
+
+
+// fetch(searchPlantURL, requestOptions)
+//   .then(response => response.json())
+//   .then(result => {
+//     if (result.data && result.data.length > 0) {
+//       displayPlants(result);
+//     } else {
+//       console.log('No results found.');
+//     }
+//   });
