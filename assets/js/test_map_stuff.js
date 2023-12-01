7aff2fe23b3ad022ea479583f7b674c52251affb
Mitchell <tmitchell@cdsvisual.com>
2023-11-30 20:39:16 -0700

    added search for zip code return hardiness

--- assets/js/test_map_stuff.js
+++ assets/js/test_map_stuff.js
@@ -1,9 +1,26 @@
+// var searchByLocationButton = document.getElementById()
+var buttonEl = $('button')
+var inputEl = $('input')
 
+const filePath = '../assets/docs/data.json'
 
-console.log("test")
+var zipCodeData = fetch(filePath).then(response => response.json()).then(data =>{
+    // console.log(data)
+    return data
+})
 
-// Reading the file using default
-// fs npm package
-import csvToJson from '../docs/node_modules/convert-csv-to-json/src/csvToJson';
-let inputFile = '../docs/phzm_us_zipcode_2023.csv'
-
+var zone = {}
+buttonEl.on('click',function(event){
+    event.preventDefault();
+    var buttonClickedID = event.target.parentNode.id
+    var input = document.getElementById(buttonClickedID).children[1].value
+   
+    
+    if (buttonClickedID && buttonClickedID == "Location_search"){
+        console.log(zipCodeData)
+        fetch(filePath).then(response => response.json()).then(data =>{
+            var zone = data.find(obj => obj['zipcode'] == input)
+            console.log(zone)
+        })
+    }
+})
