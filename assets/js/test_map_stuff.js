06a0119f3f7c7ee7884967724f496ebb17e9207a
tronmitch <theron.mitchell@gmail.com>
2023-12-03 18:44:32 -0700

    syncing

--- assets/js/test_map_stuff.js
+++ assets/js/test_map_stuff.js
@@ -10,7 +10,6 @@ const zipCodeData = fetch(filePath)
   .then(response => response.json())
   .then(data => data);
 
-<<<<<<< HEAD
 // Object to store zone data
 let zoneData = {};
 let zipCode = '';
@@ -38,7 +37,7 @@ function getZipCode(city) {
               // Find the zip code from the address components
               zipCode = addressComponents.find(component => component.types.includes('postal_code')).long_name;
               return zipCode;
-=======
+
 var zone = {}
 buttonEl.on('click',function(event){
     event.preventDefault();
@@ -58,7 +57,6 @@ console.log(i)
             if (zone.zone === i + "a" || zone.zone === i || zone.zone === i+"b"  ) {
                 var searchParameter= i; }
             sessionStorage.setItem("searchParameterKey", searchParameter)
->>>>>>> 8e714612407b8c939234030496e53b5a19d36cae
             }
           });
       }
