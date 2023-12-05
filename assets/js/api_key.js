db74272f5ea8eaef4cfa9eb29d4b7fc9408e542d
Mitchell <tmitchell@cdsvisual.com>
2023-12-05 15:27:03 -0700

    Changed plant api Key

--- assets/js/api_key.js
+++ assets/js/api_key.js
@@ -5,8 +5,9 @@
 // Will Code 1:sk-5oBn655d7588a54043071
 // Darbie code 1: sk-s0yi6566b7ea6d0b63180
 // Theron Code: sk-qWo06566be1c917ee3070
+// Theron Code: sk-BS4t656ecdaf712db3321
 // Adalia code: sk-iFmN656e96499e2713317 (FOR DEPLOYMENT)
-plantKey = "sk-5oBn655d7588a54043071";
+plantKey = "sk-BS4t656ecdaf712db3321";
 
 var requestOptions = {
     method: 'GET',
