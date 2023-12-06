7d2ddf2f1a83897d2402bd9198301efc5dcf64c9
williamrankin <william.rankin01@gmail.com>
2023-12-06 11:27:06 -0700

    sync

--- assets/js/api_key.js
+++ assets/js/api_key.js
@@ -1,6 +1,5 @@
 //Set key for accessing API
-
- // Will code 4: sk-av3F656ec9dbd65663320   
+   
 // Will code 3: sk-lgBK656bbc410c0fd3277
 // Will Code 2: sk-dEiS6566b7ba813383181
 // Will Code 1:sk-5oBn655d7588a54043071
@@ -9,7 +8,7 @@
 // Theron Code: sk-BS4t656ecdaf712db3321
 // Adalia code: sk-iFmN656e96499e2713317 (FOR DEPLOYMENT)
 
-plantKey = "sk-QBkL656f94eebc1cd3340";
+plantKey = "sk-5oBn655d7588a54043071";
 
 
 var requestOptions = {
